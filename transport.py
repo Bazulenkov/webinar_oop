9efedbdfc84de1c2f811a155fcfcd65f30b33bdc
Bazulenkov <anton@bazul.ru>
2022-07-31 22:37:53 +0400

    classmethod done

--- transport.py
+++ transport.py
@@ -1,8 +1,19 @@
 class Transport:
-    fuel = 100
+    def __init__(self, fuel) -> None:
+        self.fuel = fuel
 
-    def __init__(self, distance) -> None:
-        self.distance = distance
+    # def add_distance(self, dist):
+    #     self.distance += dist
+
+    @staticmethod
+    def beep():
+        print("fa-fa")
+
+    @classmethod
+    def class_method(cls, data):
+        fuel = data["fuel"]
+        result = cls(fuel)
+        return result
 
 
 class Car(Transport):
@@ -12,4 +23,7 @@ class Car(Transport):
 if __name__ == "__main__":
     t1 = Transport(50)
     t2 = Transport(100)
+    data = {"param1": 2, "fuel": 3, "param3": 4}
+
+    t3 = Transport.class_method(data)
     pass
