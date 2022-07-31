a7d365d233c7b27baef033ed67645bf86770be21
Bazulenkov <anton@bazul.ru>
2022-07-31 22:08:43 +0400

    .

--- transport.py
+++ transport.py
@@ -1,2 +1,15 @@
-class Transport():
+class Transport:
+    fuel = 100
+
+    def __init__(self, distance) -> None:
+        self.distance = distance
+
+
+class Car(Transport):
+    pass
+
+
+if __name__ == "__main__":
+    t1 = Transport(50)
+    t2 = Transport(100)
     pass
